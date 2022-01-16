c7028c5f37e4a462977f512ba9bd0677fd737001
S-tanishi <tanichi.u.n.20@gmail.com>
2022-01-16 19:36:14 +0900

    add: book/index で使用するcontorller , test の作成

--- apps/web/controllers/books/index.rb
+++ apps/web/controllers/books/index.rb
@@ -4,8 +4,13 @@ module Web
       class Index
         include Web::Action
 
+        # @books インスタンス変数の内容を外部に公開しviewに渡すことができる
+        expose :books
+
         def call(params)
+          @books = BookRepository.new.all
         end
+
       end
     end
   end
