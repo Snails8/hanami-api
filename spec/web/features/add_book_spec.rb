3433a854a44039a990b96a83e12e7d69b4bd4668
S-tanishi <tanichi.u.n.20@gmail.com>
2022-01-16 20:03:28 +0900

    add: user がform を送信し新しいエンティティ画策せうされるfeature test の作成

---
+++ spec/web/features/add_book_spec.rb
@@ -0,0 +1,24 @@
+require 'feature_helper'
+
+# user がtestを送信 -> 新しいエンティティが作成保存されるtest
+RSpec.describe 'ADD a book' do
+
+  before do
+    BookRepository.new.clear
+  end
+
+  it 'can create a new book' do
+    visit '/books/new'
+
+    within 'form#book-form' do
+      fill_in 'Title',  with: 'Example book'
+      fill_in 'Author', with: 'Some author'
+
+      click_button 'Create'
+    end
+
+    expect(page).to have_current_path('/books')
+    expect(page).to have_content('Example book')
+  end
+end
+
