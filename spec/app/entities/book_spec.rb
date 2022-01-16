21b9bfc8fdc879cf1ab86ee3a19b44ce0b296ec3
S-tanishi <tanichi.u.n.20@gmail.com>
2022-01-16 11:11:55 +0900

    add: books のentity の動きを確かめるためにunit testの追加

--- spec/app/entities/book_spec.rb
+++ spec/app/entities/book_spec.rb
@@ -1,3 +1,9 @@
 RSpec.describe Book, type: :entity do
-  # place your tests here
+  # This class will generate getters and setters for each attribute we pass to initialize params
+
+  it 'can be initialized with attributed' do
+    book = Book.new(title: 'Refactoring', author: 'Martin Fowler')
+    expect(book.title).to eq('Refactoring')
+    except(book.author).to eq('Martin Fowler')
+  end
 end
