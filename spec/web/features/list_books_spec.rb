ff06028ba158ca51b1c2990d6b4bb0bd83ce385a
S-tanishi <tanichi.u.n.20@gmail.com>
2022-01-16 18:34:03 +0900

    add: modelを使用した単体テストの作成

--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -3,11 +3,20 @@ require 'features_helper'
 # this test means that
 # when we go to /books, we’ll see two HTML elements that have class book, and both will be inside of an HTML element that has an id of books
 RSpec.describe 'List books' do
+  let(:repository) { BookRepository.new }
+  before do
+    repository.clear
+
+    repository.create(title: 'Practical Object-Oriented Design in Ruby', author: 'Sandi Metz')
+  end
+
   it 'displays each book on the page' do
     visit '/books'
 
     within '#books' do
-      expect(page).to have_css('.book', count: 2)
+      expect(page).to have_selector('.book', count: 1), 'Expected to find 1 book'
+      expect(page).to have_content('Practical Object-Oriented Design in Ruby')
+      expect(page).to have_content('Sandi Metz')
     end
   end
 end
